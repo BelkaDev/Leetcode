75737599b7b92be66ebab7ba599ad701ce9abab2
Belka <49416514+BelkaDev@users.noreply.github.com>
2024-02-13 12:59:36 +0100

    Time: 143 ms (5.06%) | Memory: 71.9 MB (5.06%) - LeetSync

---
+++ Easy/451-sort-characters-by-frequency/sort-characters-by-frequency.ts
@@ -0,0 +1,40 @@
+function frequencySort(s: string): string {
+    // Should use heap instead.
+    
+    
+    const freqMap = new Map();
+    const reverseMap = new Map();
+    
+    let maxFreq = 0;
+    let ans = "";
+    
+    for (const c of s) {
+        let frequency = freqMap.get(c) || 0;
+        freqMap.set(c, ++frequency);
+        
+        const prevSet = reverseMap.get(frequency-1) ?? new Set()
+        prevSet.delete(c);
+        
+        reverseMap.set(frequency-1, prevSet)
+        
+        const currSet = reverseMap.get(frequency) ?? new Set()
+        currSet.add(c);
+        reverseMap.set(frequency, currSet);
+        
+        maxFreq = Math.max(maxFreq, frequency)
+    }
+    
+    for (let i = maxFreq; i > 0; --i) {
+        const set = reverseMap.get(i);
+        for (const c of [...set]) {
+            const freq = freqMap.get(c);
+            for (let j = 0;  j < freq; j++) {
+                ans+=c
+            }
+        }
+    }
+
+    
+    return ans;
+
+};
